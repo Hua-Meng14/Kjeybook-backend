2b9950570ceeb8d1a13739088520afc709f5295b
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-06-09 00:00:30 +0700

    fix: refactor token validation isAdminToken, isUserToken, isTokenExpired

--- src/main/java/com/bootcamp/bookrentalsystem/service/JwtService.java
+++ src/main/java/com/bootcamp/bookrentalsystem/service/JwtService.java
@@ -1,20 +1,27 @@
 package com.bootcamp.bookrentalsystem.service;
 
+import com.bootcamp.bookrentalsystem.exception.BadRequestException;
+import com.bootcamp.bookrentalsystem.exception.ForbiddenException;
 import com.bootcamp.bookrentalsystem.model.User;
+import com.bootcamp.bookrentalsystem.repository.BookRepository;
+import com.bootcamp.bookrentalsystem.repository.RequestRepository;
+import com.bootcamp.bookrentalsystem.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
+import javax.swing.text.html.Option;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 @Service
 public class JwtService {
@@ -28,16 +35,16 @@ public class JwtService {
     @Value("${application.security.jwt.refresh-token.expiration}")
     private String refreshExpiration;
 
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
+    private UserRepository userRepository;
+
+    @Autowired
+    public JwtService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
+    public String generateToken(User userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
     }
-
-    public String generateToken(User userDetails) { return generateToken(new HashMap<>(), userDetails);}
 
     public String generateToken(
             Map<String, Object> extraClaims,
@@ -72,30 +79,79 @@ public class JwtService {
                 .compact();
     }
 
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
-    }
-
-    public boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseClaimsJwt(token)
-                .getBody();
-    }
-
     private SecretKey getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isAdminToken(String token) {
+        String jwtTokenWithoutInvalidChars = token.replaceAll("[^A-Za-z0-9+/=]", "");
+        byte[] tokenBytes = Base64.getUrlDecoder().decode(jwtTokenWithoutInvalidChars);
+        String decodedToken = new String(tokenBytes, StandardCharsets.UTF_8);
+        byte[] secretBytes = Decoders.BASE64.decode(decodedToken);
+
+        Jws<Claims> claimsJws = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretBytes))
+                .build()
+                .parseClaimsJws(token);
+
+        Claims claims = claimsJws.getBody();
+
+        String role = claims.get("role", String.class);
+
+        // Perform authorization logic based on the extracted information
+        return "ADMIN".equals(role);
+    }
+
+    public boolean isTokenExpired(String token) {
+        try {
+            String jwtTokenWithoutInvalidChars = token.replaceAll("[^A-Za-z0-9+/=]", "");
+            byte[] tokenBytes = Base64.getUrlDecoder().decode(jwtTokenWithoutInvalidChars);
+            String decodedToken = new String(tokenBytes, StandardCharsets.UTF_8);
+            byte[] secretBytes = Decoders.BASE64.decode(decodedToken);
+
+            Jws<Claims> claimsJws = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretBytes))
+                    .build()
+                    .parseClaimsJws(token);
+
+            Claims claims = claimsJws.getBody();
+
+            Date expiration = claims.getExpiration();
+
+            // Token expiration logic
+            return expiration.before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean isUserToken(String token, Long userId) {
+        try {
+            String jwtTokenWithoutInvalidChars = token.replaceAll("[^A-Za-z0-9+/=]", "");
+            byte[] tokenBytes = Base64.getUrlDecoder().decode(jwtTokenWithoutInvalidChars);
+            String decodedToken = new String(tokenBytes, StandardCharsets.UTF_8);
+            byte[] secretBytes = Decoders.BASE64.decode(decodedToken);
+
+            Jws<Claims> claimsJws = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretBytes))
+                    .build()
+                    .parseClaimsJws(token);
+
+            Claims claims = claimsJws.getBody();
+
+            // Extract the necessary information from the token, such as user ID or email
+            String tokenEmail = claims.get("email", String.class);
+
+            Optional<User> existingUser = userRepository.findById(userId);
+            if (existingUser.isPresent()) {
+                User user = existingUser.get();
+                // Perform authorization logic based on email
+                return tokenEmail.equals(user.getEmail());
+            }
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
