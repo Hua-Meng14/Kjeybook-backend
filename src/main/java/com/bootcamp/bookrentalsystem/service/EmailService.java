6fbb380cb1b26008bef170d3ccc1e4c23381cc5b
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-06-21 21:31:06 +0700

    feat: send email as rendered html

--- src/main/java/com/bootcamp/bookrentalsystem/service/EmailService.java
+++ src/main/java/com/bootcamp/bookrentalsystem/service/EmailService.java
@@ -86,27 +86,70 @@ public class EmailService {
         LocalDate requestDate = request.getDateOfRequest();
         String rejectedReason = request.getRejectedReason();
 
+        // Build the HTML email content
+        StringBuilder htmlContentBuilder = new StringBuilder();
+        htmlContentBuilder.append("<!DOCTYPE html>\n");
+        htmlContentBuilder.append("<html>\n");
+        htmlContentBuilder.append("<head>\n");
+        htmlContentBuilder.append("    <meta charset=\"UTF-8\">\n");
+        htmlContentBuilder.append("    <title>Book Rental Request Rejected</title>\n");
+        htmlContentBuilder.append("    <style>\n");
+        htmlContentBuilder.append("        /* CSS styling for the email */\n");
+        htmlContentBuilder.append("        body {\n");
+        htmlContentBuilder.append("            font-family: Arial, sans-serif;\n");
+        htmlContentBuilder.append("        }\n");
+        htmlContentBuilder.append("        /* Add your CSS styles here */\n");
+        htmlContentBuilder.append("    </style>\n");
+        htmlContentBuilder.append("</head>\n");
+        htmlContentBuilder.append("<body>\n");
+        htmlContentBuilder.append("    <div class=\"container\">\n");
+        htmlContentBuilder.append("        <div class=\"header\">\n");
+        htmlContentBuilder.append("            <h2>Book Rental Request Rejected</h2>\n");
+        htmlContentBuilder.append("        </div>\n");
+        htmlContentBuilder.append("        <div class=\"content\">\n");
+        htmlContentBuilder.append("            <p>Dear <strong>").append(borrower).append("</strong>,</p>\n");
+        htmlContentBuilder.append("            <p>We regret to inform you that your book rental request has been rejected. We appreciate your interest, but unfortunately, we are unable to fulfill your request at this time. Here are the details:</p>\n");
+        htmlContentBuilder.append("            <ul>\n");
+        htmlContentBuilder.append("                <li><strong>Book Title:</strong> ").append(bookTitle).append("</li>\n");
+        htmlContentBuilder.append("                <li><strong>Author:</strong> ").append(author).append("</li>\n");
+        htmlContentBuilder.append("                <li><strong>Request Date:</strong> ").append(requestDate).append("</li>\n");
+        htmlContentBuilder.append("                <li><strong>Reason:</strong> ").append(rejectedReason).append("</li>\n");
+        htmlContentBuilder.append("            </ul>\n");
+        htmlContentBuilder.append("            <p>We apologize for any inconvenience caused. If you have any further questions or concerns, please don't hesitate to reach out to our support team.</p>\n");
+        htmlContentBuilder.append("            <p>Thank you for considering our book rental service.</p>\n");
+        htmlContentBuilder.append("            <div class=\"footer\">\n");
+        htmlContentBuilder.append("                <p>Best regards,</p>\n");
+        htmlContentBuilder.append("                <p>The Book Rental Team</p>\n");
+        htmlContentBuilder.append("            </div>\n");
+        htmlContentBuilder.append("        </div>\n");
+        htmlContentBuilder.append("    </div>\n");
+        htmlContentBuilder.append("</body>\n");
+        htmlContentBuilder.append("</html>");
+
+        String htmlContent = htmlContentBuilder.toString();
+
         // build email
         // send message
-        String message = "Dear " + borrower + ",\n" +
-                "\n" +
-                "We regret to inform you that your book rental request has been rejected. We appreciate your interest, but unfortunately, we are unable to fulfill your request at this time. Here are the details:\n" +
-                "\n" +
-                "Book Title: " + bookTitle + "\n" +
-                "Author: " + author + "\n" +
-                "Request Date: " + requestDate + "\n" +
-                "Reason: " + rejectedReason + "\n" +
-                "\n" +
-                "We apologize for any inconvenience caused. If you have any further questions or concerns, please don't hesitate to reach out to our support team.\n" +
-                "\n" +
-                "Thank you for considering our book rental service.\n" +
-                "\n" +
-                "Best regards,\n" +
-                "The Book Rental Team" +
-                "" +
-                "";
+//        String message = "Dear " + borrower + ",\n" +
+//                "\n" +
+//                "We regret to inform you that your book rental request has been rejected. We appreciate your interest, but unfortunately, we are unable to fulfill your request at this time. Here are the details:\n" +
+//                "\n" +
+//                "Book Title: " + bookTitle + "\n" +
+//                "Author: " + author + "\n" +
+//                "Request Date: " + requestDate + "\n" +
+//                "Reason: " + rejectedReason + "\n" +
+//                "\n" +
+//                "We apologize for any inconvenience caused. If you have any further questions or concerns, please don't hesitate to reach out to our support team.\n" +
+//                "\n" +
+//                "Thank you for considering our book rental service.\n" +
+//                "\n" +
+//                "Best regards,\n" +
+//                "The Book Rental Team" +
+//                "" +
+//                "";
         String from = "no-reply@bookrentalsystem.com.kh";
-        send(sendTo, from, message, subject);
+//        send(sendTo, from, message, subject);
+        send(sendTo, from, htmlContent, subject);
     }
 
     public void sendResetPasswordEmail(String email, String resetPwdToken, LocalDateTime expirationTime) {
@@ -154,11 +197,12 @@ public class EmailService {
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
             MimeMessageHelper helper =
-                    new MimeMessageHelper(mimeMessage, "utf-8");
+                    new MimeMessageHelper(mimeMessage, true);
+//                    new MimeMessageHelper(mimeMessage, "utf-8");
             helper.setFrom(from);
             helper.setTo(to);
             helper.setSubject(subject);
-            helper.setText(email);
+            helper.setText(email, true);
             javaMailSender.send(mimeMessage);
         } catch (MessagingException e) {
             LOGGER.error("failed to send email", e);
